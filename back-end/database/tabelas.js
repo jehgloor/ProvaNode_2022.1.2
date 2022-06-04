ce411e2c39cbefd6c15a997bf22a0eba2515d7e8
brunomcdesa <brunotkd68@gmail.com>
2022-06-03 23:21:24 -0300

    cracao da tabela servico

--- back-end/database/tabelas.js
+++ back-end/database/tabelas.js
@@ -2,8 +2,31 @@
 class Tabelas {
 
     init(conexao){
-        console.log("Banco criado com sucesso!!")
+        console.log("Conexão com o banco iniciada")
 
         this.conexao = conexao
+        this.criaServico()
+    }
+
+    criaServico(){
+        let sql = 'create table IF NOT EXISTS servico('+
+            'id_servico INT AUTO_INCREMENT NOT NULL PRIMARY KEY,'+
+            'total DOUBLE NOT NULL,'+
+            'id_itensDeServico INT,'+
+            'id_prestadorDeServico INT,'+
+            'FOREIGN KEY (id_itensDeServico) REFERENCES itensDeServico(id_itensDeServico),'+
+            'FOREIGN KEY (id_prestadorDeServico) REFERENCES prestadorDeServico(id_prestadorDeServico))'
+           
+            this.conexao.query(sql, erro => {
+                if(erro){
+                    console.log(erro)
+                }else{
+                    console.log('Tabela SERVICO criada com sucesso!')
+                }
+
+            })
+           
     }
 }
+
+module.exports = new Tabelas;
--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,18 @@
 const customExpress = require ('./configuracao/customExpress')
+const conexao = require ('./database/connection')
+const tabelas = require('./database/tabelas')
 
-const app = customExpress()
 
-app.listen(5000,() => console.log('Servidor ativo na porta 5000'))
+
+conexao.connect(erro =>{
+    if(erro){
+        console.log('Erro na conexão com o banco'+erro)
+    }else{
+        console.log("conectado ao banco com sucesso");
+        tabelas.init(conexao)
+
+        const app = customExpress()
+        app.listen(5000,() => console.log('Servidor ativo na porta 5000'))
+    }
+})
+
