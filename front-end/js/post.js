331be10eceee5f9af1920032d3658409bfee358c
Bia <88545709+BeatrizPonciano17@users.noreply.github.com>
2022-06-16 18:50:51 -0300

    post

---
+++ front-end/js/post.js
@@ -0,0 +1,34 @@
+function fazPost(url, body){
+    let request = new XMLHttpRequest
+    request.open("POST", url, true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify(body))
+
+    request.onload = function(){
+        console.log(this.responseText)
+    }
+
+    return request.responseText
+}
+
+function ItensDeServico(){
+ //   event.preventDefault()
+    const url = 'http://localhost:5000/ItensDeServico'
+    let nome = document.getElementById("nome").value
+    let id = parseInt (document.getElementById("id").value)
+    let valor = document.getElementById("valor").value
+    let tempo = document.getElementById("tempo").value
+    console.log(nome)
+    console.log(id)
+    console.log(valor)
+    console.log(tempo)
+
+    body = {
+        "nome": nome,
+        "id": id,
+        "valor": valor,
+        "tempo": tempo
+    }
+
+    fazPost(url, body)
+}
