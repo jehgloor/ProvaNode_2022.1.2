06498e3fab44c46c6ff8377a3d7d7f31932deebd
jehgloor <89281266+jehgloor@users.noreply.github.com>
2022-06-28 11:14:54 -0300

    tirando algumas partes desnecessaria do Login

--- back-end/model/login.js
+++ back-end/model/login.js
@@ -1,66 +1,6 @@
 const conexao = require('../database/connection');
 
 class Login {
-    adiciona(login, res){
-        let sql = 'INSERT INTO login SET ?'
-
-        conexao.query(sql,login,(erro,resultado)=>{
-            if(erro){
-                res.status(400).json(erro)
-                
-            }else{
-                res.status(200).json(resultado)
-                // console.log(resultado);
-            }
-        })
-    }
-
-    lista(res){
-        const sql = 'SELECT * FROM login'
-
-        conexao.query(sql,(erro,resultado)=>{
-            if(erro){
-                res.status(400).json(erro)
-            }else{
-                res.status(200).json(resultado)
-            }
-            
-        })
-    }
-    buscaPorId(id,res){
-        let sql = "SELECT * FROM login WHERE id_login=?"
-
-        conexao.query(sql,id,(erro,resultado)=>{
-            if(erro){
-                res.status(400).json(erro)
-            }else{
-                res.status(200).json(resultado)
-            }
-        })
-    }
-    altera(id,valores,res){
-        let sql = 'UPDATE login SET ? WHERE id_login=?'
-
-        conexao.query(sql,[valores , id],(erro,resultado)=>{
-
-            if(erro){
-                res.status(400).json(erro);
-            }else{
-                res.status(200).json(resultado)
-            }
-        })
-    }
-    deletaPorId(id,res){
-        let sql = 'DELETE * FROM login WHERE id_login=?'
-
-        conexao.query(sql,id,(erro,resultado)=>{
-            if (erro){
-                res.status(400).json(erro);
-            }else{
-                res.status(200).json(resultado);
-            }
-        })
-    }
 
     buscaPorEmail(email,res){
         let sql = "SELECT email,senha FROM cliente WHERE email=?"
--- back-end/rotas/login.js
+++ back-end/rotas/login.js
@@ -2,25 +2,7 @@ const req = require("express/lib/request")
 const Login = require("../model/login")
 
 module.exports = app =>{
-    app.get('/login',(req,res) =>{
-        
-        //res.send('voce esta em Login via get')
-        Login.lista(res)
-        // console.log(res);
-    })
-
-    app.get('/login /:id',(req,res)=>{
-        let id = parseInt(req.params.id)
-        Login.buscaPorId(id,res)
-    })
-    app.patch('/login/:id',(req,res)=>{
-        let id = parseInt(req.params.id);
-        let valores = req.body
-        Login.altera(id,valores,res)
-        // aqui pode ficar aqui ?
-        //Login.deletaPorId(id,res);
-    })
-
+    
 
     app.get('/login/:tipo/:email',(req,res)=>{
         let email = req.params.email;
